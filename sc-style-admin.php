8751335cb2e89452ecb952d29eabd237cec4ec36
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-06-12 16:29:11 -0700

    Ajusted enqueue path parameter

--- sc-style-admin.php
+++ sc-style-admin.php
@@ -9,7 +9,7 @@ Author URI: https://github.com/Surfing-Chef/
 */
 
 function sc_style__admin() {
-    wp_enqueue_style('sc-style-admin', plugins_url('sc-style-admin.css', __FILE__));
+    wp_enqueue_style('sc-style-admin', plugins_url('sc-style-admin', __FILE__));
 }
 add_action('admin_enqueue_scripts', 'sc_style__admin');
 add_action('login_enqueue_scripts', 'sc_style__admin');
